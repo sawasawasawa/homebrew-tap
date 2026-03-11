356e0b97f5dc31e96bec84a61cd585d596bc22c3
mateusz sawka <m.e.sawka@gmail.com>
2026-03-11 19:25:10 +0700

    Update Diktator v1.1.0 SHA256

--- Casks/diktator.rb
+++ Casks/diktator.rb
@@ -1,6 +1,6 @@
 cask "diktator" do
   version "1.1.0"
-  sha256 "ed3422221904e2bc7bfe141a727a369848954f4185b915b943a2b92da9293c61"
+  sha256 "7f40c3d0b435d67762edf9d1ec364e78d8d4ad8f01899f2850b301d685cd9662"
 
   url "https://github.com/sawasawasawa/diktator-releases/releases/download/v#{version}/Diktator.zip"
   name "Diktator"
